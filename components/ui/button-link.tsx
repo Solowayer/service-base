fcc1739482a5d216e7ee0a2fbf6373df6543aeb3
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-08-29 21:58:45 +0300

    Initialize setup, creating buttons

---
+++ components/ui/button-link.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React, { forwardRef } from 'react'
+import Link, { LinkProps } from 'next/link'
+import { ButtonProps } from './button'
+import { Button } from './button'
+
+type ButtonLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & ButtonProps & LinkProps
+
+export const ButtonLink = forwardRef<HTMLAnchorElement, ButtonLinkProps>(function ButtonLink({ href, ...props }, ref) {
+	return (
+		<Link ref={ref} href={href} passHref>
+			<Button {...props}>{props.children}</Button>
+		</Link>
+	)
+})
---
+++ components/ui/button.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { cva, VariantProps } from 'class-variance-authority'
+import React, { forwardRef } from 'react'
+
+export const buttonStyles = cva(
+	[
+		'inline-flex items-center',
+		'relative',
+		'select-none',
+		'font-medium',
+		'disabled:bg-gray-200',
+		'disabled:text-gray-400',
+		'disabled:hover:cursor-not-allowed'
+	],
+	{
+		variants: {
+			variant: {
+				primary: 'bg-black text-white hover:bg-zinc-800 active:bg-zinc-600',
+				secondary: 'bg-white text-black hover:bg-zinc-100 active:bg-zinc-200 border',
+				danger: 'text-white bg-red-700 hover:bg-red-800 active:bg-red-900',
+				positive: 'text-white bg-green-600 hover:bg-green-700 active:bg-green-800'
+			},
+			align: {
+				start: 'justify-start',
+				center: 'justify-center',
+				end: 'justify-end'
+			},
+			size: {
+				small: 'px-3 gap-2 text-sm h-8',
+				medium: 'px-4 gap-3 text-md h-10',
+				large: 'px-5 gap-4 text-lg h-12'
+			},
+			shape: {
+				rectangle: 'rounded',
+				round: 'rounded-full',
+				square: 'rounded',
+				circle: 'rounded-full'
+			},
+			fullWidth: {
+				true: 'w-full'
+			}
+		},
+		compoundVariants: [
+			{
+				size: 'small',
+				shape: ['circle', 'square'],
+				className: '!p-2'
+			},
+			{
+				size: 'medium',
+				shape: ['circle', 'square'],
+				className: '!p-2.5'
+			},
+			{
+				size: 'large',
+				shape: ['circle', 'square'],
+				className: '!p-3'
+			}
+		],
+		defaultVariants: {
+			variant: 'primary',
+			align: 'center',
+			size: 'medium',
+			shape: 'rectangle'
+		}
+	}
+)
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonStyles> {
+	children: React.ReactNode
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+	{ variant, align, size, shape, fullWidth, children, ...props },
+	ref
+) {
+	return (
+		<button ref={ref} className={buttonStyles({ variant, align, size, shape, fullWidth })} {...props}>
+			{children}
+		</button>
+	)
+})
