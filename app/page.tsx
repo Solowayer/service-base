d1180a16456980e324f3afd645bddc1af91c505f
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-09-05 02:17:01 +0300

    More ui components (dropdown, dialog, radio-group)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 'use client'
 
+import { ComboboxInput } from '@/components/ui/combobox'
+import { useState } from 'react'
+
 export default function Home() {
-	return <main className="flex flex-col p-24"></main>
+	const [value, setValue] = useState<string>('')
+
+	return (
+		<main className="flex flex-col p-24">
+			<div className="grid grid-cols-4 gap-2">
+				<div className=""></div>
+
+				<ComboboxInput value={value} onValueChange={setValue} />
+			</div>
+		</main>
+	)
 }
--- components/ui/button-link.tsx
+++ components/ui/button-link.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { forwardRef } from 'react'
 import Link, { LinkProps } from 'next/link'
 import { ButtonProps } from './button'
